4bc01ed4f57568de208301feb408ed93335b9e09
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2021-03-11 23:28:57 +0000

    add cache tests

--- app/index.js
+++ app/index.js
@@ -4,7 +4,6 @@ const processEligibilityAction = require('./messaging/process-eligibility')
 const processDesirabiltyAction = require('./messaging/process-desirability')
 const processSubmissionAction = require('./messaging/process-submission')
 const desirabilityScoreAction = require('./messaging/desirability-score')
-const cache = require('./cache')
 
 const init = async () => {
   receivers.startEligibilityAnswersReceiver(processEligibilityAction)
@@ -14,16 +13,6 @@ const init = async () => {
 
   await server.start()
   console.log('Server running on %s', server.info.uri)
-
-  cache.initialise(server)
-
-  const key = 'testKey'
-  await cache.setDesirabilityScore(key, 'testValue')
-  console.log(`Testing get value: ${await cache.getDesirabilityScore(key)}`)
-
-  const key2 = 'testKey2'
-  await cache.setProjectDetails(key2, 'testValue2')
-  console.log(`Testing get value: ${await cache.getProjectDetails(key2)}`)
 }
 
 process.on('unhandledRejection', (err) => {
--- app/server.js
+++ app/server.js
@@ -1,13 +1,14 @@
 const Hapi = require('@hapi/hapi')
 const cacheConfig = require('./config/cache')
 const catbox = cacheConfig.useRedis ? require('@hapi/catbox-redis') : require('@hapi/catbox-memory')
+const catboxOptions = cacheConfig.useRedis ? cacheConfig.redisCatboxOptions : {}
 
 const server = Hapi.server({
   port: process.env.PORT,
   cache: [{
     provider: {
       constructor: catbox,
-      options: cacheConfig.redisCatboxOptions // FIXME: diff values for catbox memory
+      options: catboxOptions
     }
   }]
 })
