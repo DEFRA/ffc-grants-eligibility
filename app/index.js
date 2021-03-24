2f33f0667a82eba82428c9a2e4a464bd696ac6b2
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2021-03-24 10:26:00 +0000

    Remove redundant eligibility answers queue

--- app/index.js
+++ app/index.js
@@ -1,12 +1,10 @@
 const server = require('./server')
 const receivers = require('./messaging/receivers')
-const processEligibilityAction = require('./messaging/process-eligibility')
 const processDesirabiltyAction = require('./messaging/process-desirability')
 const processSubmissionAction = require('./messaging/process-submission')
 const desirabilityScoreAction = require('./messaging/desirability-score')
 
 const init = async () => {
-  receivers.startEligibilityAnswersReceiver(processEligibilityAction)
   receivers.startProjectDetailsReceiver(processDesirabiltyAction)
   receivers.startContactDetailsReceiver(processSubmissionAction)
   receivers.startDesirabilityScoreReceiver(desirabilityScoreAction)
