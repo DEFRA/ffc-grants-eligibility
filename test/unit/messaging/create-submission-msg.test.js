cf2518b14a4231be487f6d8d8a4af16861b491de
Paul Andrews <58770589+paulsimonandrews@users.noreply.github.com>
2021-04-26 23:35:12 +0100

    Hooking up json data for submission message tests

--- test/unit/messaging/create-submission-msg.test.js
+++ test/unit/messaging/create-submission-msg.test.js
@@ -1,67 +1,18 @@
 describe('Create desirability message', () => {
-  let createMsg
+  const createMsg = require('../../../app/messaging/create-submission-msg')
+  const agentSubmission = require('./submission-agent.json')
+  const farmerSubmission = require('./submission-farmer.json')
+  const desirabilityScore = require('./desirability-score.json')
 
-  beforeEach(() => {
-    // jest.mock('../../../app/config/grant-scheme', () => mockGrantScheme)
-    // jest.mock('../../../app/content-mapping', () => ({
-    //   desirabilityQuestions: mockQuestionContent,
-    //   desirabilityInputQuestionMapping: {
-    //     Q14: 'project',
-    //     Q15: 'irrigatedCrops',
-    //     Q16a: 'irrigatedLandCurrent',
-    //     Q16b: 'irrigatedLandTarget',
-    //     Q17a: 'waterSourceCurrent',
-    //     Q17b: 'waterSourcePlanned',
-    //     Q18a: 'irrigationCurrent',
-    //     Q18b: 'irrigationPlanned',
-    //     Q19: 'productivity',
-    //     Q20: 'collaboration'
-    //   }
-    // }))
-    createMsg = require('../../../app/messaging/create-submission-msg')
+  test('Farmer submission generates correct message payload', () => {
+    const msg = createMsg(farmerSubmission, desirabilityScore)
+
+    expect(msg.applicantEmail.emailAddress).toBe(farmerSubmission.farmerContactDetails.email)
   })
 
-  test('first and last name is agent if agent details included', () => {
-    expect(true).toBeDefined()
+  test('Agent submission generates correct message payload', () => {
+    const msg = createMsg(agentSubmission, desirabilityScore)
+
+    expect(msg.applicantEmail.emailAddress).toBe(agentSubmission.agentContactDetails.email)
   })
-
-  // test('first and last name is agent if agent details included', () => {
-  //   const submission = {
-  //     agentDetails: {
-  //       firstName: 'Joe',
-  //       lastName: 'Smith'
-  //     },
-  //     farmerDetails: {
-  //       firstName: 'Pippa',
-  //       lastName: 'Jones'
-  //     }
-  //   }
-
-  //   const msg = createMsg(submission, {})
-
-  //   expect(msg.firstName).toBeDefined()
-  //   expect(msg.lastName).toBeDefined()
-  //   expect(msg.firstName).toBe(submission.agentDetails.firstName)
-  //   expect(msg.lastName).toBe(submission.agentDetails.lastName)
-  // })
-
-  // test('first and last name is farmer if agent details not included', () => {
-  //   const submission = {
-  //     agentDetails: {
-  //       firstName: null,
-  //       lastName: null
-  //     },
-  //     farmerDetails: {
-  //       firstName: 'Pippa',
-  //       lastName: 'Jones'
-  //     }
-  //   }
-
-  //   const msg = createMsg(submission, {})
-
-  //   expect(msg.firstName).toBeDefined()
-  //   expect(msg.lastName).toBeDefined()
-  //   expect(msg.firstName).toBe(submission.farmerDetails.firstName)
-  //   expect(msg.lastName).toBe(submission.farmerDetails.lastName)
-  // })
 })
